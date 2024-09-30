e53f28dbd23e12ed05289d82264eab19354c816d
Valerie Cabrera Mays <a20213391@pucp.edu.pe>
2024-09-30 18:25:34 -0500

    Parametros para graficar

--- Zieger.ino
+++ Zieger.ino
@@ -13,31 +13,31 @@
 // Number of motors
 #define NMOTORS 4
 // Pins
-const int enc[] = {4, 5, 8, 13};
-const int DIR[] = {2, 7, 9, 12};
-const int pwm[] = {3, 6, 10, 11};
+const int enc[] = { 4, 5, 8, 13 };
+const int DIR[] = { 2, 7, 9, 12 };
+const int pwm[] = { 3, 6, 10, 11 };
 
 // Globals
-float vel   = 0.0;
-long prevT  = 0;
+float vel = 0.0;
+long prevT = 0;
 // PPR of each motor
-const double ppr[] = {1390, 1390, 1390, 1390}; 
+const double ppr[] = { 1390, 1390, 1390, 1390 };
 
 // Dynamic variables
-int velEnc[]     = {0, 0, 0, 0};
-int velEncSlack  = 0;
+int velEnc[] = { 0, 0, 0, 0 };
+int velEncSlack = 0;
 
 // Variables
-int   input  = 255;
-int   dir[]  = {1, 1, 1, 1};
-float t0     = 0.0;
-long  initT  = 0;
+int input = 255;
+int dir[] = { 1, 1, 1, 1 };
+float t0 = 0.0;
+long initT = 0;
 
 /*----------------------- YOU CAN MODIFY THIS -----------------------*/
-int   motor   = 1;   // From 1 to 4.
-float waitT   = 2.0; // In seconds.
-float totalT  = 4.0; // In seconds.
-float sampleT = 0.1; // In seconds.
+int motor = 1;        // From 1 to 4.
+float waitT = 1.0;    // In seconds.
+float totalT = 3.0;  // In seconds.
+float sampleT = 0.01;  // In seconds.
 /*-------------------------------------------------------------------*/
 
 /*********************************************************************/
@@ -45,7 +45,7 @@ float sampleT = 0.1; // In seconds.
 /*********************************************************************/
 
 void setup() {
-  int k = motor-1;
+  int k = motor - 1;
   // Begin communication
   Serial.begin(9600);
   // Setup I/O pins
@@ -60,7 +60,7 @@ void setup() {
   delay(2000);
   prevT = micros();
   initT = prevT;
-  t0 = ((float) (initT))/( 1.0e6 );
+  t0 = ((float)(initT)) / (1.0e6);
   Serial.print(0);
   Serial.print(",");
   Serial.println(0.0);
@@ -71,38 +71,38 @@ void setup() {
 /*********************************************************************/
 
 void loop() {
-  int k = motor-1;
+  int k = motor - 1;
   // Time difference
   long currT = micros();
-  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
-  float t1 = ((float) (currT))/( 1.0e6 );
-  
+  float deltaT = ((float)(currT - prevT)) / (1.0e6);
+  float t1 = ((float)(currT)) / (1.0e6);
+
   // Compute program each sample time
-  if(sampleT <= deltaT){
+  if (sampleT <= deltaT) {
     prevT = currT;
     // Disable interrupts temporarily while reading
-    noInterrupts(); 
+    noInterrupts();
     // Reset counter
     velEncSlack = velEnc[k];
     velEnc[k] = 0;
     interrupts();
     // Calculate angular velocity in rpm
-    vel = velEncSlack/deltaT/ppr[k]*60.0;
+    vel = velEncSlack / deltaT / ppr[k] * 60.0;
 
     // Wait time
-    if((t1-t0) < waitT){
+    if ((t1 - t0) < waitT) {
       StopMotors();
-      Serial.print(currT- initT);
+      Serial.print(currT - initT);
       Serial.print(",");
       Serial.println(vel);
     }  // Run time
-    else if((t1-t0) < totalT){
+    else if ((t1 - t0) < totalT) {
       setMotor(dir[k], input, pwm[k], DIR[k]);
       Serial.print(currT - initT);
       Serial.print(",");
       Serial.println(vel);
-    } // End
-    else{
+    }  // End
+    else {
       StopMotors();
     }
   }
@@ -123,7 +123,7 @@ void setMotor(int dir, int pwmVal, int pwmch, int dirch) {
     - dirch: Direction pin channel.
   */
   analogWrite(pwmch, pwmVal);
-  if(dirch==12 || dirch==7){
+  if (dirch == 12 || dirch == 7) {
     if (dir == 1) {
       digitalWrite(dirch, LOW);
     } else if (dir == 0) {
@@ -131,8 +131,7 @@ void setMotor(int dir, int pwmVal, int pwmch, int dirch) {
     } else {
       digitalWrite(dirch, LOW);
     }
-  }
-  else{
+  } else {
     if (dir == 1) {
       digitalWrite(dirch, HIGH);
     } else if (dir == 0) {
@@ -144,7 +143,7 @@ void setMotor(int dir, int pwmVal, int pwmch, int dirch) {
 }
 
 
-template <int j>
+template<int j>
 void readEncoder() {
   /* 
   Function that counts each rising edge of a encoder
@@ -152,11 +151,11 @@ void readEncoder() {
   velEnc[j]++;
 }
 
-void StopMotors(){
+void StopMotors() {
   /* 
   Function that stops each DC motor. 
   */
-  for(int k = 0; k < NMOTORS; k++){
+  for (int k = 0; k < NMOTORS; k++) {
     setMotor(dir[k], 0.0, pwm[k], DIR[k]);
   }
 }
